6c9f22f8a12db4bd8cc23386412991a9133851ba
Alourne <120846738+Alourne@users.noreply.github.com>
2023-02-13 20:08:06 -0500

    did divs in js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hi")
+document.addEventListener("DOMContentLoaded" , function() {
+    createBoard(20);
+    console.log("hi");
+})
+
+function createBoard(size) {
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
